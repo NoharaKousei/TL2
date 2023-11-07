04becb13fcaf404dab653a81900b50a4903a7d5f
nohara_kousei <k021g1159@g.neec.ac.jp>
2023-11-07 15:02:15 +0900

    02_04

--- TextureConverter.cpp
+++ TextureConverter.cpp
@@ -13,7 +13,7 @@ void TextureConverter::LoadWICTextureFromFile(const std::string& filePath)
 
 std::wstring TextureConverter::ConvertMultiByteStringToWideString(const std::string& mString)
 {
-	//ワイド文字列に変換した際の文字数を計算
+	//ワイド文字列に変換した際の文字数を計算 
 	int filePathBufferSize = MultiByteToWideChar(CP_ACP, 0, mString.c_str(), -1, nullptr, 0);
 
 	//ワイド文字列
